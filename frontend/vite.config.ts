921e59794609a24a509854ab82a310261fb2e430
IanDalton <daltioan@gmail.com>
2025-08-10 06:02:28 -0300

    remove the base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // If deploying to GitHub Pages project site (https://<user>.github.io/BetterResume/)
 // we need base to match repo name. We use GITHUB_PAGES env flag set in workflow.
-const repoBase = '/BetterResume/';
 
 export default defineConfig(() => ({
-  base: process.env.GITHUB_PAGES ? repoBase : '/',
   plugins: [react()],
 }));
